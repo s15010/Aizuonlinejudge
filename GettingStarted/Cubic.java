504fea9ebef4fd48ca780cb0ed973f2d7693ab8a
s15010 <s15010@std.it-college.ac.jp>
2016-05-24 15:05:57 +0900

    commit

---
+++ GettingStarted/Cubic.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Cubic{
+    public static void main(String[]agrs){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.println(x*x*x);
+     }
+}
